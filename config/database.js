b1d719ea65dbd8ae80d3dc3812365550288d2589
MOUADBelhora <68823481+MOUADBelhora@users.noreply.github.com>
2024-05-15 17:16:01 +0100

    edit some config to lik strapi with clover database

--- config/database.js
+++ config/database.js
@@ -1,51 +1,61 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'mysql');
 
   const connections = {
     mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'bg4wqz5t9lw09mcqargp-mysql.services.clever-cloud.com'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'bg4wqz5t9lw09mcqargp'),
+        user: env('DATABASE_USERNAME', 'uekuszyyi4t1r7hb'),
+        password: env('DATABASE_PASSWORD', '5Relzcq5qMoKv3iuBYSR'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: 0,
+        max: 5,
+        acquireTimeoutMillis: 30000, // 30 seconds
+        createTimeoutMillis: 30000, // 30 seconds
+        idleTimeoutMillis: 30000,   // 30 seconds
+        reapIntervalMillis: 1000,   // 1 second
+        createRetryIntervalMillis: 200 // 200 ms
+      },
     },
     mysql2: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'bg4wqz5t9lw09mcqargp-mysql.services.clever-cloud.com'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'bg4wqz5t9lw09mcqargp'),
+        user: env('DATABASE_USERNAME', 'uekuszyyi4t1r7hb'),
+        password: env('DATABASE_PASSWORD', '5Relzcq5qMoKv3iuBYSR'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: 0,
+        max: 5,
+        acquireTimeoutMillis: 30000, // 30 seconds
+        createTimeoutMillis: 30000, // 30 seconds
+        idleTimeoutMillis: 30000,   // 30 seconds
+        reapIntervalMillis: 1000,   // 1 second
+        createRetryIntervalMillis: 200 // 200 ms
+      },
     },
     postgres: {
       connection: {
@@ -61,22 +71,23 @@ module.exports = ({ env }) => {
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: 0,
+        max: 10,
+        acquireTimeoutMillis: 30000, // 30 seconds
+        createTimeoutMillis: 30000, // 30 seconds
+        idleTimeoutMillis: 30000,   // 30 seconds
+        reapIntervalMillis: 1000,   // 1 second
+        createRetryIntervalMillis: 200 // 200 ms
+      },
     },
     sqlite: {
       connection: {
-        filename: path.join(
-          __dirname,
-          '..',
-          env('DATABASE_FILENAME', '.tmp/data.db')
-        ),
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
@@ -86,7 +97,7 @@ module.exports = ({ env }) => {
     connection: {
       client,
       ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000), // 60 seconds
     },
   };
 };
